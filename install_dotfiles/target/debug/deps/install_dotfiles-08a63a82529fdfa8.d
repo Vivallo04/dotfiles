9d4ffc30aa1a4334cce9ba6ccba1c4a198423f31
Vivallo <andresvivallo4@gmail.com>
2021-08-26 11:07:36 -0600

    Add Rust script
    Still needs some more work

---
+++ install_dotfiles/target/debug/deps/install_dotfiles-08a63a82529fdfa8.d
@@ -0,0 +1,5 @@
+/home/vivallo/Documents/dotfiles/install_dotfiles/target/debug/deps/install_dotfiles-08a63a82529fdfa8: src/main.rs
+
+/home/vivallo/Documents/dotfiles/install_dotfiles/target/debug/deps/install_dotfiles-08a63a82529fdfa8.d: src/main.rs
+
+src/main.rs:
---
+++ install_dotfiles/target/debug/install_dotfiles.d
@@ -0,0 +1,1 @@
+/home/vivallo/Documents/dotfiles/install_dotfiles/target/debug/install_dotfiles: /home/vivallo/Documents/dotfiles/install_dotfiles/src/main.rs
